3220547a9c07741bb485f7db10d9279024f03b8c
Mauricio Menezes <mauriciosm95@gmail.com>
2022-01-12 02:52:10 -0300

    Atualiza data de encontro

--- portfolio/contagem-regressiva-ver-caju/js/script.js
+++ portfolio/contagem-regressiva-ver-caju/js/script.js
@@ -1,6 +1,6 @@
 import Coutdown from "./countdown.js";
 
-const tempoParaVerCaju = new Coutdown("13 January 2022 10:00:00 GMT-0300");
+const tempoParaVerCaju = new Coutdown("14 January 2022 10:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function atualizarTempo() {
